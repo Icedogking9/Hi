edc3aae6f5d473fe62e54dfcec9095c36530174a
LoganTackett1 <logantackettwork@gmail.com>
2023-11-21 01:50:57 -0800

    working player dir for model and camera

--- src/index.js
+++ src/index.js
@@ -2,23 +2,20 @@ import * as THREE from 'three';
 import css from './index.css';
 import { generateUUID } from 'three/src/math/MathUtils';
 import { initFetch } from './comm.js';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-// setting up player data and what not
-
-const playerId = generateUUID();
-
-const initialServerData = initFetch();
+// basic scene set up
 
 const scene = new THREE.Scene();
 scene.background = new THREE.Color(0xA9FFFF);
 const camera = new THREE.PerspectiveCamera(90, window.innerWidth/window.innerHeight, 0.1, 1000);
-camera.position.set(5,5,5);
+camera.position.set(2,0,2);
 camera.lookAt(0,0,0);
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight, false);
 document.body.appendChild( renderer.domElement );
 
-const light = new THREE.AmbientLight( 0x404040 ); // soft white light
+const light = new THREE.AmbientLight( 0xF0F0F0 ); // soft white light
 light.position.set(10,10,10);
 scene.add( light );
 
@@ -31,7 +28,40 @@ const plane = new THREE.Mesh( geometry, material );
 plane.rotateX(Math.PI/2);
 scene.add( plane );
 
+// player set up
+
+const loader = new GLTFLoader();
+let playerModel;
+
+loader.load( './blender/player.glb', function ( gltf ) {
+    scene.add( gltf.scene );
+    playerModel = gltf;
+}, undefined, function ( error ) {
+    console.error( error );
+} );
+
+const playerObj = {dir:{theta:0,delta:0},pos:{x:0,y:0,z:0}};
+
+
+const playerId = generateUUID();
+
+const initialServerData = initFetch();
+
+const modelEuler = new THREE.Euler(0,0,0);
+const cameraEuler = new THREE.Euler(0,0,0);
 function gameFrame () {
+    if (playerModel.scene) {
+        playerModel.scene.position.set(playerObj.pos.x,playerObj.pos.y+0.33,playerObj.pos.z);
+        modelEuler.set(0,playerObj.dir.theta+Math.PI/2,0);
+        playerModel.scene.setRotationFromEuler(modelEuler);
+        cameraEuler.set(playerObj.dir.delta,playerObj.dir.theta,0,'ZYX');
+        camera.setRotationFromEuler(cameraEuler);
+        const cameraOffset = new THREE.Vector3(2/3,2/3,1);
+        const rotatedCameraOffset = cameraOffset.applyEuler(cameraEuler);
+        camera.position.set(playerObj.pos.x+rotatedCameraOffset.x,
+                            playerObj.pos.y+0.33+rotatedCameraOffset.y,
+                            playerObj.pos.z+rotatedCameraOffset.z);
+    }
     renderer.render( scene, camera );
 }
 
